ba8b76c4013c17ec16bf4a257c850da2e1bf32be
S.Listl <s.listl@adito.de>
2021-10-12 10:55:55 +0200

    style changes

--- modules/flowable-ui-modeler/flowable-ui-modeler-app/src/main/resources/static/scripts/app.js
+++ modules/flowable-ui-modeler/flowable-ui-modeler-app/src/main/resources/static/scripts/app.js
@@ -202,7 +202,7 @@ flowableModeler
                     'id': 'processes',
                     'title': 'GENERAL.NAVIGATION.PROCESSES',
                     'path': '/processes'
-                },
+                }/*,
                 {
                     'id': 'casemodels',
                     'title': 'GENERAL.NAVIGATION.CASEMODELS',
@@ -217,12 +217,12 @@ flowableModeler
                     'id': 'decision-tables',
                     'title': 'GENERAL.NAVIGATION.DECISION-TABLES',
                     'path': '/decision-tables'
-                }
-                //,{
-                //    'id': 'apps',
-                //    'title': 'GENERAL.NAVIGATION.APPS',
-                //    'path': '/apps'
-                //}
+                },
+                {
+                    'id': 'apps',
+                    'title': 'GENERAL.NAVIGATION.APPS',
+                    'path': '/apps'
+                }*/
             ];
 
             $rootScope.config = FLOWABLE.CONFIG;
