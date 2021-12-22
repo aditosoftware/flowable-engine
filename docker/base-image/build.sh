90d2b7e39849c664af6de986b59f507eb8aa5e58
Sebastian Listl <s.listl@adito.de>
2021-12-22 13:27:24 +0100

    dockerfile

--- docker/base-image/build.sh
+++ docker/base-image/build.sh
@@ -1,1 +1,40 @@
-docker build -t flowable/java8_server:latest -t flowable/java8_server:8.181 .
+#!/bin/sh
+dir >> log.txt
+BASE_PWD="$PWD"
+FLOWABLE_VERSION=6.5.0
+
+echo "Initializing for version $FLOWABLE_VERSION"
+
+mkdir -p $BASE_PWD/assets && rm -f $BASE_PWD/assets/*.original
+
+echo "Building Flowable IDM"
+cd ../../modules/flowable-ui-idm
+mvn -T 1C clean install -DskipTests -Pdocker-deps
+STATUS=$?
+if [ $STATUS -eq 0 ]
+then
+    echo "Copying artifact"
+	cp flowable-ui-idm-app/target/*.original $BASE_PWD/assets/
+else
+    echo "Error while building root pom. Halting."
+fi
+
+cd $BASE_PWD
+
+echo "Building Flowable Modeler"
+cd ../../modules/flowable-ui-modeler
+mvn -T 1C clean install -DskipTests -Pdocker-deps
+STATUS=$?
+if [ $STATUS -eq 0 ]
+then
+    echo "Copying artifact"
+	cp flowable-ui-modeler-app/target/*.original $BASE_PWD/assets/
+else
+    echo "Error while building root pom. Halting."
+fi
+
+cd $BASE_PWD
+
+echo "Building Docker image for version: $FLOWABLE_VERSION"
+
+docker build -t adito/flowable:2021.2.3 .
